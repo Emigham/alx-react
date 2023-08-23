b52f8fef47f98bddd2271d63cd2c41143748f65b
Emigham <emigham7@gmail.com>
2023-08-23 03:29:06 +0100

    extra lessons

---
+++ hello-world/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Jack" heroName="Warrio">
+        <p>This is children props</p>
+        </Greet>
+      <Greet name="Marck" heroName="Actor">
+        <button>Action</button>
+        </Greet>
+      <Greet name="Pharez" heroName="Breakthrough"/>
+       <Welcome name="Jack" heroName="Warrio" />
+       <Welcome  name="Marck" heroName="Actor"/>
+       <Welcome  name="Pharez" heroName="Breakthrough"/>
+      {/*<Hello /> */}
+    </div>
+  )
+}
+
+export default App;
